69ffeeb6167c58e80ed838af6a253f8de260b7fd
Damon Murray <damon.murray@agworld.com.au>
2017-10-07 13:05:42 +0800

    Mount react viewport component

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Viewport from './viewport';
+
+const divId = 'react-soroban';
+const div = document.createElement('div');
+div.id = divId;
+
+document.body.appendChild(div);
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Viewport),
+    document.getElementById(divId)
+  );
+});
---
+++ src/viewport.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Viewport = () => {
+  return (
+    <div id='react-soroban-viewport'>
+      <div>Soroban will display here</div>
+      <div>Score / Value will display here</div>
+    </div>
+  );
+};
+
+export default Viewport;
